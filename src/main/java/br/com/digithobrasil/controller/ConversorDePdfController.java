b0c87ad6ac31d2db0a530cae6ac719d17c08b00f
Vinícius Saueia <vsaueia@gmail.com>
2016-09-09 15:12:12 -0400

    Conversão de txt para pdf

--- src/main/java/br/com/digithobrasil/controller/ConversorDePdfController.java
+++ src/main/java/br/com/digithobrasil/controller/ConversorDePdfController.java
@@ -1,7 +1,7 @@
 package br.com.digithobrasil.controller;
 
-import com.itextpdf.text.Document;
-import com.itextpdf.text.Image;
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.PdfWriter;
 import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
@@ -9,33 +9,36 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 
 @SpringBootApplication
-@RestController
+@RestController(value = "/v1")
 public class ConversorDePdfController {
 
     private static final Logger LOG = Logger.getLogger(ConversorDePdfController.class);
 
-    @RequestMapping(value = "/arquivo", method = RequestMethod.POST)
-    public @ResponseBody byte[] converterArquivo (@RequestParam(value="file", required=true) MultipartFile file ) {
+    @RequestMapping(value = "/converter", method = RequestMethod.POST)
+    public @ResponseBody byte[] converterArquivo (@RequestParam(value="arquivo", required=true) MultipartFile arquivoOriginal ) {
 
         try {
-            Document document = new Document();
             try {
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                PdfWriter writer = PdfWriter.getInstance(document, baos);
-                writer.open();
-                document.open();
-                document.add(Image.getInstance(file.getBytes()));
-                document.close();
-                writer.close();
-                return baos.toByteArray();
+                ByteArrayOutputStream pdfConvertido;
+                if (ehArquivoDeTexto(arquivoOriginal)) {
+                    pdfConvertido = conveterTxtParaPdf(arquivoOriginal.getBytes());
+                } else {
+                    pdfConvertido = converterImagemParaPdf(arquivoOriginal.getBytes());
+                }
+
+                // passo temporario só para teste
+                escreverNoDisco(pdfConvertido.toByteArray());
+
+                return pdfConvertido.toByteArray();
             }
             catch (Exception e) {
                 e.printStackTrace();
@@ -50,9 +53,60 @@ public class ConversorDePdfController {
         return null;
     }
 
+    private boolean ehArquivoDeTexto(@RequestParam(value = "arquivo", required = true) MultipartFile arquivoOriginal) {
+        return arquivoOriginal.getOriginalFilename().endsWith("txt");
+    }
+
+    private ByteArrayOutputStream converterImagemParaPdf(byte[] bytes) throws DocumentException, IOException {
+        Document document = new Document();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PdfWriter writer = PdfWriter.getInstance(document, baos);
+        writer.open();
+        document.open();
+        document.add(Image.getInstance(bytes));
+        document.close();
+        writer.close();
+        return baos;
+    }
+
+    private ByteArrayOutputStream conveterTxtParaPdf(byte[] bytes) throws Exception {
+        Document document = new Document(PageSize.A4);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PdfWriter writer = PdfWriter.getInstance(document, baos);
+        writer.open();
+        document.open();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(bytes)));
+        String strLine;
+        BaseFont courier = BaseFont.createFont(BaseFont.COURIER, BaseFont.CP1252, BaseFont.EMBEDDED);
+        Font myfont = new Font(courier);
+        while ((strLine = br.readLine()) != null) {
+            Paragraph para = new Paragraph(strLine + "\n", myfont);
+            para.setAlignment(Element.ALIGN_JUSTIFIED);
+            document.add(para);
+        }
+
+        document.close();
+        writer.close();
+        return baos;
+    }
+
+    private void escreverNoDisco(byte[] bytes) throws IOException {
+        OutputStream outStream = null;
+        ByteArrayOutputStream byteOutStream = null;
+        try {
+            outStream = new FileOutputStream("/home/vsaueia/Desktop/fileNameToSave.pdf");
+            byteOutStream = new ByteArrayOutputStream();
+            byteOutStream.write(bytes);
+            byteOutStream.writeTo(outStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            outStream.close();
+        }
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ConversorDePdfController.class, args);
     }
-
 }
